7687a5a95da49070bc0fb7d41f60220602f0a377
OniAndria254 <154087569+OniAndria254@users.noreply.github.com>
2024-05-07 14:26:14 +0300

    commit change script

--- script1.bat
+++ script1.bat
@@ -1,11 +1,15 @@
+if errorlevel 1 (
+        pause
+        exit /b 1
+)
+
 set src_dir=C:/e-bossy/S4/Web Dyn/framework/framework/src
 set temp_src=C:/e-bossy/S4/Web Dyn/framework/framework/temp-src
 
 set bin_dir=C:/e-bossy/S4/Web Dyn/framework/framework/bin
-set lib_dir=C:/e-bossy/S4/Web Dyn/hello-spring/lib
+set lib_dir=C:/e-bossy/S4/Web Dyn/framework/framework/lib/servlet-api.jar
 set work_dir=C:/e-bossy/S4/Web Dyn/framework/framework
 
-mkdir "%temp_dir%"
 mkdir "%temp_src%"
 
 FOR /R "%src_dir%" %%F IN (*.java) DO (
@@ -13,6 +17,7 @@ FOR /R "%src_dir%" %%F IN (*.java) DO (
 )
 
 cd "%temp_src%"
-javac -sourcepath "%temp_src%" -d "%bin_dir%" *.java
+javac -sourcepath "%temp_src%" -d "%bin_dir%" -cp "%lib_dir%" *.java
 
 jar -cvf "%work_dir%/myServlet.jar" -C "%bin_dir%" . 
+
--- src/FrontControllerServlet.java
+++ src/FrontControllerServlet.java
@@ -1,38 +1,28 @@
 package mg.itu.prom16.controller;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FrontControllerServlet extends HttpServlet {
-    private List<String> urls = new ArrayList<>();
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String url = request.getRequestURI();
-        urls.add(url); // Ajouter l'URL à la liste
-
-        response.setContentType("text/html");
+        throws ServletException, IOException {
+        String url = request.getRequestURL().toString();
+ 
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>URLs saisies :</h1>");
-        for (String u : urls) {
-            out.println("<p>" + u + "</p>");
-        }
-        out.println("</body></html>");
+        out.println(url);
     }
 
-    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }

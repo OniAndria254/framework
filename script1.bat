3714340558deb3932645d5805e79803100bd8cf0
OniAndria254 <154087569+OniAndria254@users.noreply.github.com>
2024-05-07 11:25:17 +0300

    commit javax

--- script1.bat
+++ script1.bat
@@ -2,7 +2,7 @@ set src_dir=C:/e-bossy/S4/Web Dyn/framework/framework/src
 set temp_src=C:/e-bossy/S4/Web Dyn/framework/framework/temp-src
 
 set bin_dir=C:/e-bossy/S4/Web Dyn/framework/framework/bin
-set lib_dir=C:/e-bossy/S4/Web Dyn/framework/framework/lib
+set lib_dir=C:/e-bossy/S4/Web Dyn/hello-spring/lib
 set work_dir=C:/e-bossy/S4/Web Dyn/framework/framework
 
 mkdir "%temp_dir%"
@@ -13,6 +13,6 @@ FOR /R "%src_dir%" %%F IN (*.java) DO (
 )
 
 cd "%temp_src%"
-javac -sourcepath "%temp_src%" -d "%bin_dir%" -cp "%lib_dir%/*" *.java
+javac -sourcepath "%temp_src%" -d "%bin_dir%" *.java
 
 jar -cvf "%work_dir%/myServlet.jar" -C "%bin_dir%" . 

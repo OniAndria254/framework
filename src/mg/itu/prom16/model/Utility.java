2156f27375defec72eaa7b2d7871d5e08916163e
OniAndria254 <154087569+OniAndria254@users.noreply.github.com>
2024-09-27 19:37:30 +0300

    Final Sprint 9

--- src/mg/itu/prom16/model/Utility.java
+++ src/mg/itu/prom16/model/Utility.java
@@ -4,11 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import java.util.Enumeration;
+import java.util.HashMap;
 
 public class Utility {
+    private static final Gson gson = new Gson();
     public static boolean isPrimitive(Class<?> clazz) {
         // Liste des types primitifs en Java
         List<Class<?>> primitiveTypes = Arrays.asList(
@@ -26,6 +31,29 @@ public class Utility {
         return primitiveTypes.contains(clazz);
     }
 
+    public static String objectToJson(Object obj) {
+        MyJson myJson = new MyJson();
+        return myJson.getGson().toJson(obj);
+    }
+
+    // Méthode pour convertir une chaîne JSON en objet Java
+    public static <T> T jsonToObject(String jsonString, Class<T> clazz) {
+        MyJson myJson = new MyJson();
+        return myJson.getGson().fromJson(jsonString, new TypeToken<T>(){}.getType());
+    }
+
+    public static String modelViewToJson(ModelView modelView) {
+        MyJson myJson = new MyJson();
+        return myJson.getGson().toJson(modelView.getData());
+    }
+
+    // Méthode pour transformer les données du ModelView en JSON séparé
+    public static String modelViewDataToJson(ModelView modelView) {
+        HashMap<String, Object> data = modelView.getData();
+        MyJson myJson = new MyJson();
+        return myJson.getGson().toJson(data);
+    }
+    
     public static String capitalize(String str) {
         char firstChar = str.charAt(0);
         return Character.toUpperCase(firstChar) + str.substring(1);

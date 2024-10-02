4b7329fc62679b38c9932e682acf4fb406af70b9
OniAndria254 <154087569+OniAndria254@users.noreply.github.com>
2024-10-02 16:01:23 +0300

    Sprint 10 ok

--- src/mg/itu/prom16/controller/FrontControllerServlet.java
+++ src/mg/itu/prom16/controller/FrontControllerServlet.java
@@ -109,6 +109,9 @@ public class FrontControllerServlet extends HttpServlet {
                 Method vraiMethod = null;
                 // Vérification si une méthode avec le nom spécifié existe
                 for (Method method : declaredMethods) {
+                    if (!mapping.getVerb().equalsIgnoreCase(request.getMethod())) {
+                        throw new ServletException("Url et Verb non compatibles");
+                    }
                     if (method.getName().equals(mapping.getMethode())) {
                         vraiMethod = method;
                         break;
@@ -170,38 +173,37 @@ public class FrontControllerServlet extends HttpServlet {
                 }
 
                 // Ajouter la vérification de l'annotation Restapi ici
-            if (vraiMethod.isAnnotationPresent(Restapi.class)) {
-                // Récupérer la valeur de retour de la méthode
-                Object result = vraiMethod.invoke(controllerInstance, args);
-                
-                if (result instanceof ModelView) {
-                    ModelView modelView = (ModelView) result;
+                if (vraiMethod.isAnnotationPresent(Restapi.class)) {
+                    // Récupérer la valeur de retour de la méthode
+                    Object result = vraiMethod.invoke(controllerInstance, args);
                     
-                    // Transformer les données en JSON
-                    String jsonData = Utility.modelViewToJson(modelView);
+                    if (result instanceof ModelView) {
+                        ModelView modelView = (ModelView) result;
+                        
+                        // Transformer les données en JSON
+                        String jsonData = Utility.modelViewToJson(modelView);
+                        
+                        // Configurer le type de réponse comme text/json
+                        response.setContentType("text/json");
+                        
+                        // Écrire la réponse
+                        PrintWriter pw = response.getWriter();
+                        pw.print(jsonData);
+                    } else {
+                        // Transformer directement en JSON si ce n'est pas un ModelView
+                        String jsonData = Utility.objectToJson(result);
+                        
+                        // Configurer le type de réponse comme text/json
+                        response.setContentType("text/json");
+                        
+                        // Écrire la réponse
+                        PrintWriter pw = response.getWriter();
+                        pw.print(jsonData);
+                    }
                     
-                    // Configurer le type de réponse comme text/json
-                    response.setContentType("text/json");
-                    
-                    // Écrire la réponse
-                    PrintWriter pw = response.getWriter();
-                    pw.print(jsonData);
-                } else {
-                    // Transformer directement en JSON si ce n'est pas un ModelView
-                    String jsonData = Utility.objectToJson(result);
-                    
-                    // Configurer le type de réponse comme text/json
-                    response.setContentType("text/json");
-                    
-                    // Écrire la réponse
-                    PrintWriter pw = response.getWriter();
-                    pw.print(jsonData);
+                    // Arrêter l'exécution après avoir envoyé la réponse JSON
+                    return;
                 }
-                
-                // Arrêter l'exécution après avoir envoyé la réponse JSON
-                return;
-            }
-        
 
                 // Invocation de la méthode
                 Object result = vraiMethod.invoke(controllerInstance, args);
